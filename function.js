be54a9eccd8380883d725ef1531951b9bdbe0abb
PaulChaeJeonghyun <89195896+PaulChaeJeonghyun@users.noreply.github.com>
2022-07-07 23:05:27 -0400

    js function 2

--- function.js
+++ function.js
@@ -60,3 +60,42 @@ function printAll(...args) {
    args.forEach((arg) => console.log(arg));
 }
 printAll('dream', 'coding', 'paul');
+
+// 5. Local scope
+let globalMessage = 'global'; //global variable
+function printMessage() {
+   let message = 'hello';
+   console.log(message); // local variable
+   console.log(globalMessage);
+   function printAnother() {
+      console.log(message);
+      let childMessage = 'hello';
+   }
+}
+printMessage();
+//console.log(message);
+//console.log(childMessage); //error
+
+// 6. Return a value
+function sum(a, b) {
+   return a + b;
+}
+const result = sum(1, 2); //3
+console.log(`sum: ${sum(1, 2)}`);
+
+// 7. Early return, early exit
+// bad
+function upgradeUser(user) {
+   if (user.point > 10) {
+      //long upgrade logic...
+   }
+}
+
+// good
+function upgradeUser(user) {
+   if (user.point <= 10) {
+      return;
+   }
+   //long upgrade logic...
+}
+
