4a035e45b28da57541490669bd52deff70f178a9
PaulChaeJeonghyun <89195896+PaulChaeJeonghyun@users.noreply.github.com>
2022-07-13 13:11:02 -0400

    json2

--- json.js
+++ json.js
@@ -34,3 +34,22 @@ console.log(json);
 
 // 2. JSON to Object
 // parse(json)
+console.clear();
+json = JSON.stringify(rabbit);
+const obj = JSON.parse(json, (key, value) => {
+   console.log(`key: ${key}, value: ${value}`);
+   return key === 'birthDate' ? new Date(value) : value;
+});
+console.log(obj);
+rabbit.jump();
+//obj.jump();
+
+
+console.log(rabbit.birthDate.getDate());
+console.log(obj.birthDate);
+console.log(obj.birthDate.getDate());
+
+//jsondiff.com
+//jsonbeatifier
+//jsonparser.org
+//jsonvalidator.com
